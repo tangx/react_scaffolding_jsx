29d6b308dd0abef83d8cc3050cef52eef9e56c8b
Frank (Xin) Tang <f.tang@aligntech.com>
2022-09-16 00:13:46 +0800

    高亮 item

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
 
 export default class Item extends Component {
+
+  state = {
+    mouse: false,
+  }
+
+  // 函数柯里化
+  handleMouse = (flag) => {
+    return () => {
+      this.setState({ mouse: flag })
+    }
+  }
   render() {
     const { todo } = this.props
+    const { mouse } = this.state
     return (
-      <li>
+      <li
+        onMouseEnter={this.handleMouse(true)}
+        onMouseLeave={this.handleMouse(false)}
+        style={{ backgroundColor: mouse ? '#ddd' : 'white' }}
+
+      >
         <input type="checkbox" defaultChecked={todo.done} />
         <span>{todo.name}</span>
-        <button>删除</button>
+        <button hidden={!mouse}>删除1</button>
       </li>
     )
   }
