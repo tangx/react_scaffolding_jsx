8df24ca16fde0c6d4e22d85b58058f6f553501b9
Frank (Xin) Tang <f.tang@aligntech.com>
2022-09-16 14:23:08 +0800

    clean

--- src/App.js
+++ src/App.js
@@ -2,104 +2,16 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-import List from './components/List';
 
 export default class App extends Component {
 
 
-  state = {
-    todos: [
-      { id: '001', name: "吃饭", done: true },
-      { id: '002', name: "睡觉", done: false },
-      { id: '003', name: "吃饭", done: false },
-      { id: '004', name: "逛街", done: true },
-    ]
-  }
-
-  updateTodos = (id, checked) => {
-    // console.log(id, checked);
-
-    const { todos } = this.state
-
-    const newTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        // 使用 ES6 语法「复制对象字面量， 并修改某字段」
-        return { ...todo, done: checked }
-      }
-      return todo
-    })
-
-    this.setState({ todos: newTodos })
-  }
-
-
-  deleteTodo = (id) => {
-    const { todos } = this.state
-
-    const newTodos = todos.filter((item) => {
-      return item.id !== id
-    })
-
-    this.setState({ todos: newTodos })
-  }
-
-  addItem = (todoObj) => {
-    // console.log("App:", data);
-
-    // // 获取当前时间戳
-    // const ts = new Date().getTime().toString()
-    // const todo = { id: ts, name: todoObj, done: false }
-
-    this.setState(
-      {
-        // 在最前面增加 todo item
-        todos: [todoObj, ...this.state.todos]
-      },
-    )
-  }
-
-  allTodosChecked = (checked) => {
-    const { todos } = this.state
-
-    const newTodos = todos.map((todo) => {
-      return { ...todo, done: checked }
-    })
-
-    this.setState(
-      { todos: newTodos }
-    )
-  }
-
-  deleteCheckedTodos = () => {
-    const { todos } = this.state
-
-    const newTodos = todos.filter((todo) => {
-      if (!todo.done) {
-        return todo
-      }
-    })
-
-    this.setState({ todos: newTodos })
-  }
 
   render() {
 
-    const { todos } = this.state
     return (
       <div className='App'>
-        <span>TodoList</span>
-        <Header addItem={this.addItem} />
-        <List todos={todos}
-          updateTodos={this.updateTodos}
-          deleteTodo={this.deleteTodo}
-        />
-        <Footer todos={this.state.todos}
-          allTodosChecked={this.allTodosChecked}
-          deleteCheckedTodos={this.deleteCheckedTodos}
-        />
-
+        APP
       </div>
     )
   }
